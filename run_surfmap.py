3818add5d25f571f8b191066c79a041f97132ce2
nchenche <nicolas.chevrollier@laposte.net>
2021-10-14 21:02:07 +0200

    Update parser helps

--- run_surfmap.py
+++ run_surfmap.py
@@ -19,21 +19,16 @@ OPTIONAL_ARGUMENTS = {
 DATA_VOLUME_PATH = "/home/surfmap/data/"  # volume mount point in docker container
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-pdb", required = True, help = "input pdb file (path + file name)")
-parser.add_argument(
-    "-tomap",
-    type = str,
-    required = True,
-    choices = set(("all", "stickiness", "kyte_doolittle", "wimley_white", "electrostatics", "circular_variance", "circular_variance_atom", "bfactor", "binding_sites")),
-    help = "Property to map. Argument must be one of the following: stickiness, kyte_doolittle, wimley_white, electrostatics, circular_variance, bfactor")
-parser.add_argument("-coords", required = False, help = "file with coordinates to point on maps. Must be of the following format: protein; phi; theta.")
-parser.add_argument("-res", required = False, help = "file containing a list of residues to map on the projection. Format must be the following: col 1 = chain id; col 2 = res number; col 3 = res type")
-parser.add_argument("-rad", required = False, help = "radius added to usual atomic radius (used for calculation solvent excluded surface). The higher the radius the smoother the surface (default = 3.0 Angstrom)")
-parser.add_argument("-d", required = False, help = "output directory where all files will be written. Defaults = MappedValues in current directory.")
-parser.add_argument("-s", required = False, help = "size of a grid cell. Necessary that 180%%cellsize == 0. Default = 5.")
-parser.add_argument("--nosmooth", required = False, action = "store_true", help = "If chosen, the resulted maps are not smoothed (careful: this option should be used only for discrete values!)")
-parser.add_argument("--png", required = False, action = "store_true", help = "option to compute map in png format (default: pdf format)")
-parser.add_argument("--keep", required = False, action = "store_true", help = "option to keep all intermediary files (default is to keep only final txt matrix and pdf map)")
+parser.add_argument("-pdb",required = True, help = "Input pdb file (path + file name)")
+parser.add_argument("-tomap", type = str, required = True, choices = set(("all", "stickiness", "kyte_doolittle", "wimley_white", "electrostatics", "circular_variance", "circular_variance_atom", "bfactor", "binding_sites")), help = "Choice of the scale. Argument must be one of the following: stickiness; kyte_doolittle; wimley_white; electrostatics; circular_variance; bfactor; binding_sites; all")
+parser.add_argument("-coords", help = argparse.SUPPRESS)
+parser.add_argument("-res", help = "File containing a list of residues to map on the projection. Format must be the following: col 1 = chain id; col 2 = res number; col 3 = res type")
+parser.add_argument("-rad", required = False, help = "Radius in Angstrom added to usual atomic radius (used for calculation solvent excluded surface). The higher the radius the smoother the surface (default: 3.0)")
+parser.add_argument("-d", required = False, help = "Output directory where all files will be written (default: './output_SURFMAP_$pdb_$tomap' where $pdb and $tomap are the inputs given to -pdb and -tomap arguments, respectiveley)")
+parser.add_argument("-s", required = False, help = "Size of a grid cell. Necessary that 180%%cellsize == 0 (default: 5.0)")
+parser.add_argument("--nosmooth", action = "store_true", help = "If chosen, the resulted maps are not smoothed (careful: this option should be used only for discrete values!)")
+parser.add_argument("--png", action = "store_true", help = "If chosen, a map in png format is computed (default: only pdf format is generated)")
+parser.add_argument("--keep", action = "store_true", help = "If chosen, all intermediary files are kept in the output (default: only final text matrix and pdf map are kept)")
 args = parser.parse_args()
 
 pdbarg = args.pdb
