eb5a732eaf579914eb86aa9fc7f06ff75480fba9
nchenche <nicolas.chevrollier@laposte.net>
2022-02-28 15:28:29 +0100

    Update dockerfile and docker image User can now update its latest local image of surfmap with 'python run_surfmap.py --update'

--- run_surfmap.py
+++ run_surfmap.py
@@ -2,12 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import argparse, os, subprocess
+import docker
 
-DOCKER_REPOSITORY = 'nchenche/'
-IMAGE_TAG = 'surfmap:latest'
-DOCKER_IMAGE = DOCKER_REPOSITORY + IMAGE_TAG
+DOCKER_REPOSITORY = 'nchenche/surfmap'
+IMAGE_TAG = 'latest'
+DOCKER_IMAGE = DOCKER_REPOSITORY + ':' + IMAGE_TAG
 
 OPTIONAL_ARGUMENTS = {
+    'proj': '-proj',
     'coords': '-coords',
     'res': '-res',
     'rad': '-rad',
@@ -18,9 +20,31 @@ OPTIONAL_ARGUMENTS = {
     }
 DATA_VOLUME_PATH = "/home/surfmap/data/"  # volume mount point in docker container
 
+update_parser = argparse.ArgumentParser()
+update_parser.add_argument("--update", required=False, action="store_true", help = "Update your docker image version")
+update_args = update_parser.parse_args()
+
+if update_args.update:
+    try:
+        client = docker.client.from_env()
+    except:
+        print('Error, it seems like the docker daemon service is not running...')
+        print('If you have docker installed on your machine, you can start your docker service as follows:')
+        print(' - on linux system type in  terminal "sudo service docker start"')
+        print(' - on a Windows or macOS system, simply open your docker desktop app GUI')
+
+    try:
+        print('Updating your local SURFMAP docker image with the latest availabe in {}'.format(DOCKER_IMAGE))
+        client.images.pull(repository=DOCKER_REPOSITORY, tag=IMAGE_TAG)
+        print('Updating process done. You are now using the latest version of SURFMAP\n')
+        exit()
+    except:
+        print('Error while trying to pull the latest docker image in {}'.format(DOCKER_IMAGE))
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-pdb",required = True, help = "Input pdb file (path + file name)")
 parser.add_argument("-tomap", type = str, required = True, choices = set(("all", "stickiness", "kyte_doolittle", "wimley_white", "electrostatics", "circular_variance", "circular_variance_atom", "bfactor", "binding_sites")), help = "Choice of the scale. Argument must be one of the following: stickiness; kyte_doolittle; wimley_white; electrostatics; circular_variance; bfactor; binding_sites; all")
+parser.add_argument("-proj", type = str, required = False, choices = set(("sin", "moll", "aitoff", "cyl")), help = "Choice of the projection. Argument must be one of the following: sinusoidal; mollweide; aitoff; cylequalarea")
 parser.add_argument("-coords", help = argparse.SUPPRESS)
 parser.add_argument("-res", help = "File containing a list of residues to map on the projection. Format must be the following: col 1 = chain id; col 2 = res number; col 3 = res type")
 parser.add_argument("-rad", required = False, help = "Radius in Angstrom added to usual atomic radius (used for calculation solvent excluded surface). The higher the radius the smoother the surface (default: 3.0)")
