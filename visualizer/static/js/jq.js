28526fb124524e15ab0f15f425a3466bad6e374f
nchenche <nicolas.chevrollier@laposte.net>
2022-03-10 22:59:25 +0100

    Change visualizer name to viewer

--- visualizer/static/js/jq.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    
-    $('#svg-container').on('click', function(event) {
-        if (event.target.tagName == 'rect') {
-            console.log(event.target);
-        }
-    });
-
-});
